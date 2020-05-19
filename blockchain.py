8c23f643c4aec30b3b08bd49bf2d2fe509669b27
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-19 17:47:53 +0100

    Experimenting with lists

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,11 @@
-print('hello world')
+blockchain = [1]
+
+
+def add_value(value):
+    blockchain.append([blockchain[-1], value])
+    print(blockchain)
+
+
+add_value(5)
+add_value(2)
+add_value(4)
