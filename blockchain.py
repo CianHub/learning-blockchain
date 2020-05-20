0d2484b80a2d3a74d11d812c3d422ca080212d86
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-20 13:17:40 +0100

    Add basic verification for blockchain

--- blockchain.py
+++ blockchain.py
@@ -28,6 +28,24 @@ def print_blockchain_blocks():
         print(block)
 
 
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        # Checks if the first element of a block is equal to the entire previous block
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+
+    return is_valid
+
+
 while True:
     print('Please Choose:')
     print('1: add a new transaction value')
@@ -35,18 +53,26 @@ while True:
     print('q: quit')
     user_choice = get_user_choice()
 
-    if user_choice is '1':
+    if user_choice == '1':
         transaction_value = get_transaction_value(),
         add_transaction(transaction_value, get_last_blockchain_value())
 
-    elif user_choice is '2':
+    elif user_choice == '2':
         print_blockchain_blocks()
 
-    elif user_choice is 'q':
+    elif user_choice == 'h':
+        if len(blockchain):
+            blockchain[0] = 2
+
+    elif user_choice == 'q':
         break
 
     else:
         print('Input was invalid, please enter 1 or 2')
+
+    if not verify_chain():
+        print('Invalid blockchain')
+        break
 
     print('Choice registered')
 
