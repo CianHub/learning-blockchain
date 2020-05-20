06127010297759691414caf08eec19ab2ceb700b
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-20 12:07:12 +0100

    Revise conditionals

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,13 @@
 blockchain = []
 
 
-def get_user_input():
+def get_transaction_value():
     return int(input('Enter a number: '))
+
+
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
 
 def get_last_blockchain_value():
@@ -13,9 +18,23 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([transaction_amount, last_transaction])
 
 
-add_value(get_user_input())
-
-while True:
-    add_value(get_user_input(), get_last_blockchain_value())
+def print_blockchain_blocks():
     for block in blockchain:
         print(block)
+
+
+transaction_value = get_transaction_value()
+add_value(transaction_value)
+
+while True:
+    print('Please Choose:')
+    print('1: add a new transaction value')
+    print('2: Output blockchain blocks')
+    user_choice = get_user_choice()
+
+    if user_choice == '1':
+        transaction_value = get_transaction_value(),
+        add_value(transaction_value, get_last_blockchain_value())
+
+    else:
+        print_blockchain_blocks()
