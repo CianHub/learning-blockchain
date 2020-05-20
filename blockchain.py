10d93b2d2dbff2a78f978683b2c1672b0a46607d
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-20 11:46:29 +0100

    Revising loops and while

--- blockchain.py
+++ blockchain.py
@@ -11,9 +11,11 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([transaction_amount, last_transaction])
-    print(blockchain)
 
 
 add_value(get_user_input())
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=3)
-add_value(get_user_input(), [2])
+
+while True:
+    add_value(get_user_input(), get_last_blockchain_value())
+    for block in blockchain:
+        print(block)
