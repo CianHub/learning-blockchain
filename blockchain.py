58866e52f3e32adfc4f6ff75b4de2eaec1eb5e8f
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-21 18:14:57 +0100

    Improve hashing with list comprehensions

--- blockchain.py
+++ blockchain.py
@@ -32,18 +32,13 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 
 def hash_block(last_block):
-    hash_block = ''
-
-    for key in last_block:
-        value = last_block[key]
-        hash_block = hash_block + str(value)
-
-    return hash_block
+    return '-'.join([str(last_block[key]) for key in last_block])
 
 
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    print(hashed_block)
 
     block = {
         'previous_hash': hashed_block,
