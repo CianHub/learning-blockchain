8318b38e5370f8cc2f4d426cb9385722f4d78d61
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-28 13:06:21 +0100

    Add wallet class, create functionality for generating public and private keys

---
+++ wallet.py
@@ -0,0 +1,17 @@
+import Crypto
+import binascii
+
+
+class Wallet:
+    def __init__(self):
+        private_key, public_key = self.generate_keys()
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def generate_keys(self):
+        # Generate a private key with the RSA algorithim to be 1024 bits
+        private_key = Crypto.PublicKey.RSA.generate(
+            1024, Crypto.Random.new().read())
+        public_key = private_key.public_key()
+        # keys are in binary so need to convert them
+        return (binascii.hexlify(private_key.export_key(format='DER')).decode('ascii'), binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
