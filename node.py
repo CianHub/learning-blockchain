697d2db7d773222e1343fb61988f0f56ca5c31b2
Cian O'Gralaigh <ihatecian@gmail.com>
2020-05-30 18:50:58 +0100

    Create blockchain route

--- node.py
+++ node.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 from wallet import Wallet
+from blockchain import Blockchain
 
 # Pass Flask the Python file to establish the context in which it runs
 app = Flask(__name__)
 
 flask_wallet = Wallet()
+blockchain = Blockchain(flask_wallet.public_key)
 
 # Enable Cross-Origin Resource sharing
 CORS(app)
@@ -19,6 +21,19 @@ def get_ui():
     return 'Its working!'
 
 
-# Will only launch server if node.py is run in its own context
+@app.route('/chain', methods=['GET'])
+def get_blockchain():
+    chain = blockchain.chain
+    # iterate through blockchain and get a dict for each block
+    dict_chain = [block.__dict__.copy() for block in chain]
+
+    # iterate through each block and their transactions and convert each transaction to a dict
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [transaction.__dict__.copy()
+                                      for transaction in dict_block['transactions']]
+    return jsonify(dict_chain), 200
+
+
+    # Will only launch server if node.py is run in its own context
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
